63795f9ab92e0c74ff9eb704a3615b81e22074e3
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-08-12 08:02:51 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import ErrorPage from './pages/ErrorPage';
 
 
 function App() {
+
+  // the nav is showing on every single page 
+  // the error page is always at the bottom and astris it
   return (
    <Router>
+     <nav>
+       <Link to="/">Home</Link>
+       <Link to="/About"> About</Link>
+       <Link to="/Profile">Profile</Link>
+     </nav>
      <Routes>
-      <Route path="/"  />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="•" element={<ErrorPage />}/>
      </Routes>
    </Router>
   );
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div>ErrorPage hello</div>
+  )
+}
+
+export default ErrorPage
